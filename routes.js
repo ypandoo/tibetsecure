1a296f19f449ef092d083736b098b1b65f93e087
ypandoo <ypandoo@126.com>
2017-11-23 14:34:43 +0800

    update info page

---
+++ routes.js
@@ -0,0 +1,9 @@
+var Routes = {
+  'G109':{
+            'StartPt': { "latitude": 94.967877, "longtitude": 36.385471},
+            'EndPt': { "latitude": 90.8578, "longtitude": 29.721328 },
+            'ZoomPt': { "latitude": 91.091126, "longtitude": 30.08676 },
+            'Markers': [{ 'title': '西大滩', 'description': '西大滩乡位于天祝藏族自治县北部毛毛山北麓，东邻东大滩乡，西连朵什乡，南与华藏寺镇毗邻，北与古浪县接壤，面积187平方千米', 'distance': '3000', "latitude": 94.312966, "longtitude": 35.748759},
+              { 'title': '沱沱河', 'description': '沱沱河介绍', 'distance': '3000', "latitude": 92.458791, "longtitude": 34.234271}]
+          }
+}
